a8c1991f0fbcd6138b94271ec6536d8cedab31c4
ellinhoms9 <126779141+ellinhoms9@users.noreply.github.com>
2023-03-14 19:23:25 +0200

    task 1-3 is done

--- HW4/task1-3.py
+++ HW4/task1-3.py
@@ -1,4 +1,5 @@
 users_text = input("Please input the text: ")
+# check if the text is a number
 if users_text.isdigit():
     print("This is a number.")
     if int(users_text) == 0:
@@ -7,6 +8,7 @@ if users_text.isdigit():
         print("This number is even.")
     else:
         print("This number is odd.")
+# check if the text is a word
 elif users_text.isalpha():
     print(f"This is a word. Length of this word is {len(users_text)}.")
 else:
