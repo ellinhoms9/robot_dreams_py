4b95537ffcdebf7087eac3d3fb8a3edc8796cb96
ellinhoms9 <126779141+ellinhoms9@users.noreply.github.com>
2023-03-31 15:43:03 +0300

    task 4 is changed

--- HW8/task4.py
+++ HW8/task4.py
@@ -1,7 +1,6 @@
 def filter_int(elem):
     # check type of elements
-    if type(elem) == int or type(elem) == float:
-        return elem
+    return isinstance(elem, (int, float))
 
 
 data = [1, "a", 2.1, "B", 3, "c", 4.7, "d", 5]
