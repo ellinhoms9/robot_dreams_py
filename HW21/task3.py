ed14ed309eb466af56fdc359b24f7547cd0a5465
ellinhoms9 <126779141+ellinhoms9@users.noreply.github.com>
2023-05-23 21:47:04 +0300

    task 3 is done

---
+++ HW21/task3.py
@@ -0,0 +1,13 @@
+import sqlite3
+
+connection = sqlite3.connect("my_database.db")
+cursor = connection.cursor()
+
+query = ("CREATE TABLE users ("
+         "id INTEGER PRIMARY KEY AUTOINCREMENT,"
+         "first_name TEXT NOT NULL,"
+         "last_name TEXT NOT NULL,"
+         "age INTEGER)")
+
+
+cursor.execute(query)
