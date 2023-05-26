be99dee123d461a1e6bb0fa3a330181bf165031c
ellinhoms9 <126779141+ellinhoms9@users.noreply.github.com>
2023-05-26 12:01:53 +0300

    task 2 is done

---
+++ HW22/task2.py
@@ -0,0 +1,10 @@
+import sqlite3
+from pprint import pprint
+
+conn = sqlite3.connect("database_1.sqlite")
+cursor = conn.cursor()
+
+query = "SELECT COUNT(id) FROM users WHERE age > 30"
+
+res = cursor.execute(query)
+pprint(res.fetchall())
