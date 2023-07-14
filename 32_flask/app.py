9a9e6a166c3a2eddbead43e2e707427e7d979415
ellinhoms9 <126779141+ellinhoms9@users.noreply.github.com>
2023-07-14 11:54:06 +0300

    updated app

--- 32_flask/app.py
+++ 32_flask/app.py
@@ -1,28 +1,24 @@
+import logging
 from flask import Flask
-from logging.config import dictConfig
+from config import AppConfig
+from flask_sqlalchemy import SQLAlchemy
 
-dictConfig({
-    'version': 1,
-    'formatters': {'default': {
-        'format': '[%(asctime)s] %(levelname)s in %(module)s: %(message)s',
-    }},
-    'handlers': {'wsgi': {
-        'class': 'logging.StreamHandler',
-        'stream': 'ext://flask.logging.wsgi_errors_stream',
-        'formatter': 'default'
-    }},
-    'root': {
-        'level': 'INFO',
-        'handlers': ['wsgi']
-    }
-})
 
+db = SQLAlchemy()
 app = Flask(__name__)
-app.secret_key = 'some-secret-key'
+app.logger.setLevel(logging.INFO)
+app.config.from_object(AppConfig)
+db.init_app(app)
 
 
 from views import *
+from models import *
 
+
+with app.app_context():
+    db.create_all()
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5001)
+    app.run(host=app.config.get('HOST'),
+            port=app.config.get('PORT'),
+            debug=app.config.get('DEBUG'))
