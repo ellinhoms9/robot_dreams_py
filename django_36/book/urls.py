1376bde0e148d504985626f65206db9a20f43a77
ellinhoms9 <126779141+ellinhoms9@users.noreply.github.com>
2023-08-08 00:19:20 +0300

    add new urls for all applications

--- django_36/book/urls.py
+++ django_36/book/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from book import views
 
+app_name = 'books'
+
 urlpatterns = [
-    path('', views.all_books)
+    path('', views.BookListView.as_view(), name='books-list'),
+    path('<int:pk>', views.BookDetailView.as_view(), name='books-detail'),
+    path('create/', views.BooksCreateView.as_view(), name='books-create')
 ]
--- django_36/users/urls.py
+++ django_36/users/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from users import views
 
+app_name = 'users'
+
 urlpatterns = [
-    path('', views.users_all)
+    path('', views.UsersListView.as_view(), name='users-list'),
+    path('<int:pk>', views.UsersDetailView.as_view(), name='users-detail'),
+    path('create/', views.UsersCreateView.as_view(), name='users-create')
 ]
