e74086e9a822f15c21391ba3cdd8134550fe339f
ellinhoms9 <126779141+ellinhoms9@users.noreply.github.com>
2023-08-08 00:17:21 +0300

    changes admin.py files

--- django_36/book/admin.py
+++ django_36/book/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from book.models import Books
 
-# Register your models here.
+
+@admin.register(Books)
+class BooksAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'author', 'year', 'price')
+    search_fields = ('title', 'author')
--- django_36/purchase/admin.py
+++ django_36/purchase/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from purchase.models import Purchases
 
-# Register your models here.
+
+@admin.register(Purchases)
+class PurchasesAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'book', 'date')
+    search_fields = ('user__first_name', 'user__last_name', 'book__title')
