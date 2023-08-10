a37be59596146d98d93dfd48cf6b4d2f0abdd6df
ellinhoms9 <126779141+ellinhoms9@users.noreply.github.com>
2023-08-10 14:21:06 +0300

    changed urls

--- django_36/book/urls.py
+++ django_36/book/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from book import views
+from rest_framework.routers import SimpleRouter
+from book.api import views as api_views
 
 app_name = 'books'
 
@@ -8,3 +10,8 @@ urlpatterns = [
     path('<int:pk>', views.BookDetailView.as_view(), name='books-detail'),
     path('create/', views.BooksCreateView.as_view(), name='books-create')
 ]
+
+router = SimpleRouter()
+router.register('api', api_views.BookModelViewSet)
+
+urlpatterns += router.urls
--- django_36/purchase/urls.py
+++ django_36/purchase/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from purchase import views
+from rest_framework.routers import SimpleRouter
+from purchase.api import views as api_views
 
 app_name = 'purchases'
 
@@ -8,3 +10,8 @@ urlpatterns = [
     path('<int:pk>', views.PurchaseDetailView.as_view(), name='purchases-detail'),
     path('create/', views.PurchaseCreateView.as_view(), name='purchases-create')
 ]
+
+router = SimpleRouter()
+router.register('api', api_views.PurchaseModelViewSet)
+
+urlpatterns += router.urls
